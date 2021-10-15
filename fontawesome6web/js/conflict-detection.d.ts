efc322fbb6b9d12761689f9c0ff4a8efeffd327c
Akhilesh Boodhun <quantum19992011@gmail.com>
2021-10-15 10:19:15 +0400

    Setup TS env + Nodesass

---
+++ fontawesome6web/js/conflict-detection.d.ts
@@ -0,0 +1,12 @@
+export = md5;
+/**
+ * Calculates MD5 value for a given string.
+ * If a key is provided, calculates the HMAC-MD5 value.
+ * Returns a Hex encoded string unless the raw argument is given.
+ *
+ * @param {string} string Input string
+ * @param {string} [key] HMAC key
+ * @param {boolean} raw Raw oytput switch
+ * @returns {string} MD5 output
+ */
+declare function md5(string: string, key?: string, raw: boolean): string;
